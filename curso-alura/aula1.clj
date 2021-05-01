f9980fa922cf40d2c1a355d791cfd37908eceea0
phcbarros <paulo.barros@outlook.com.br>
2021-05-01 17:02:50 -0300

    Curso Alura - Clojure: Introdução à programação funcional - 1 Primeiras funções - 03 Funções e vetores def

---
+++ curso-alura/aula1.clj
@@ -0,0 +1,20 @@
+(println "Bem vindo ao sistema de estoque")
+
+(def total-de-produto 15)
+(println "Total:" total-de-produto)
+
+(println (+ 5 5))
+
+(def total-de-produto (+ total-de-produto 5))
+(println "Novo Total:" total-de-produto)
+
+(def estoque ["PS4", "Notebook"])
+(println estoque)
+
+(println (estoque 0))
+(println (estoque 1))
+(println (conj estoque "Mouse"))
+(println "Valor atual de estoque" estoque)
+
+(def estoque (conj estoque "Mouse"))
+(println "Valor atualizado de estoque" estoque)
