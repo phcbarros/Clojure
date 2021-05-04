638a974ec3052ecf1912988a69ad2e2c5bf06980
phcbarros <paulo.barros@outlook.com.br>
2021-05-04 19:33:44 -0300

    Curso Alura - Clojure: Introdução à programação funcional - 03 Funções anonimas e lambdas - 04 Funções como parâmetros, funções anonimas e lambdas

--- curso-alura/src/curso_alura/aula3.clj
+++ curso-alura/src/curso_alura/aula3.clj
@@ -50,3 +50,39 @@
 
 (println (aplica-desconto? 1000))
 (println (aplica-desconto? 100))
+
+(println "Passsando função como parâmetro")
+
+
+(defn mais-caro-que-100?
+  [valor-bruto]
+  (< valor-bruto 100))
+
+(defn valor-descontado
+  "Aplica desconto de 10% se deve aplicar desconto"
+  [aplica?, valor-bruto]
+  (if (aplica? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (println  "Taxa de desconto" taxa-de-desconto)
+      (println  "Desconto" desconto)
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado mais-caro-que-100? 1000))
+(println (valor-descontado mais-caro-que-100?, 100))
+
+(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 1000))
+(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 100))
+(println (valor-descontado (fn [v] (> v 100)) 1000))
+(println (valor-descontado (fn [v] (> v 100)) 100))
+(println (valor-descontado #(> %1 100) 1000))               ;%2....
+(println (valor-descontado #(> %1 100) 100))
+(println (valor-descontado #(> % 100) 1000))
+(println (valor-descontado #(> % 100) 100))
+
+(def mais-caro-que-100? (fn [valor-bruto] (> valor-bruto 100)))
+(def mais-caro-que-100? #(> % 100 ))
+
+(println (valor-descontado mais-caro-que-100? 1000))
+(println (valor-descontado mais-caro-que-100? 100))
